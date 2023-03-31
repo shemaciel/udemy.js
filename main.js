f26d95f5a55899b60f898a6980dedaeaf57eab43
Sheron <103598166+shemaciel@users.noreply.github.com>
2023-03-31 10:26:37 -0300

    primeiros passos
    aprendendo js e css

---
+++ main.js
@@ -0,0 +1,51 @@
+/*
+1. Aula 
+console.log('Ola');
+console.log('Sou Sheron');
+console.log('Eu tenho 34 anos');*/
+
+ /*
+ 2. Aula
+ console.log(primeiroNome);
+console.log(idade);
+console.log(sobrenoma);*/
+
+/*3. Aula
+
+let primeiroNome = 'Sheron';
+let sobrenome = 'Maciel';
+let idade = 28;
+
+console.log('Meu nome é ' + primeiroNome);
+console.log('Eu tenho ' + idade + ' anos de idade');
+*/
+
+ /*
+ 4.Aula
+ let primeiroNome = prompt('Digite seu primeiro nome:');
+ let sobrenome = prompt('Digite seu segundo nome;');
+ let idade = prompt('Digite qual sua idade:');
+
+ console.log(primeiroNome);
+ console.log(sobrenome);
+ console.log(idade);*/
+
+ /*5. Aula
+
+ let primeiroNome = prompt('Digite seu primeiro nome: ');
+ let sobrenome = prompt('Digite seu segundo nome; ');
+ let idade = parseInt(prompt('Digite qual sua idade: ')) ; //variavel que pode ser modificada
+ const idade2 = 2; // variavel que não se pode modificar em momento algum no seu codigo.
+
+ //idade2 = 4;
+ idade = idade + 2;
+
+ console.log(primeiroNome);
+ console.log(sobrenome);
+ console.log(idade);
+*/
+
+/*6. Aula
+
+falando de css*/
+ 
